29dfb61edaaf8b8d0fe1064bb6d24424ccfaae35
crazy_piggy_ <57442208+crazy-piggy@users.noreply.github.com>
2021-02-08 22:39:55 -0600

    Create 排序.cs

---
+++ 排序.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace asd
+{
+    
+}
+//定义数组
+//            int[] array = new int[10];
+　　　　 　　　//输出最大值
+//             int max = array.Max();
+             //输出最小值
+//             int min = array.Min();
+//             Console.WriteLine("\n最大值：{0}，最小值：{1}",max,min);
