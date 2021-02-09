695e2a4138cee52bcf422953079a9e2adc5ddffa
crazy_piggy_ <57442208+crazy-piggy@users.noreply.github.com>
2021-02-08 22:47:00 -0600

    Update 排序.cs

--- 排序.cs
+++ 排序.cs
@@ -5,7 +5,7 @@ namespace asd
     
 }
 //定义数组
-//            int[] array = new int[10];
+//            int[] array = new int[10] {1,2,3,4,5,6,7,8,9,0};
 　　　　 　　　//输出最大值
 //             int max = array.Max();
              //输出最小值
